8578fb061157f854eb996fa2baaa406fed902214
Svetlozar Kirkov <svetlozark@gmail.com>
2015-02-23 22:59:14 +0200

    cleaned branch

--- scripts/audio.js
+++
@@ -1,6 +0,0 @@
-var nakov = new Audio();
-var marinov = new Audio();
-var peshev = new Audio();
-nakov.src = 'voices/nakov2.wav';
-marinov.src = 'voices/marinov.wav';
-peshev.src = 'voices/peshev2.wav';
