966d6df3a9266ae8002f8fe3233f54f7fc3692d9
Svetlozar Kirkov <svetlozark@gmail.com>
2015-02-23 15:58:21 +0200

    added onclick audio in trainers.html

---
+++ scripts/audio.js
@@ -0,0 +1,6 @@
+var nakov = new Audio();
+var marinov = new Audio();
+var peshev = new Audio();
+nakov.src = 'voices/nakov2.wav';
+marinov.src = 'voices/marinov.wav';
+peshev.src = 'voices/peshev2.wav';
